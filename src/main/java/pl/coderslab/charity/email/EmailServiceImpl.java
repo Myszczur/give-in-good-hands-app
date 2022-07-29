5afac7f05bc77e7efbf0e25c982f11301c225a92
Kamil <Kamil.Urbanik@o2.pl>
2022-07-29 20:53:22 +0200

    v1.2.1 - add email sending

---
+++ src/main/java/pl/coderslab/charity/email/EmailServiceImpl.java
@@ -0,0 +1,23 @@
+package pl.coderslab.charity.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sendSimpleMessage(
+            String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@goodhands.pl");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
